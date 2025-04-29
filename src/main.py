a1efaf4a90b1e04f518bf9bd0ad748133469a2df
analucia-odp <analucia.odp@gmail.com>
2025-04-28 21:42:21 -0300

    7. Executando Todos os Testes & 8. Comandos Assert

--- src/main.py
+++ src/main.py
@@ -92,8 +92,23 @@ from base.test_runner import TestRunner
 # suite.run(result)
 # print(result.summary())
 
+# loader = TestLoader()
+# suite = loader.make_suite(TestLoaderTest)
+
+# runner = TestRunner()
+# runner.run(suite)
+
+# ------- Questão 7 - 8 -------
+
 loader = TestLoader()
-suite = loader.make_suite(TestLoaderTest)
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
 
 runner = TestRunner()
 runner.run(suite)
