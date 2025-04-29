abc55b2def9723a44a8c740fcc74141c16cc56d2
analucia-odp <analucia.odp@gmail.com>
2025-04-28 21:27:20 -0300

    5. Classe TestSuite

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
 from tests.my_test import MyTest
 from base.test_result import TestResult
 from tests.test_case_test import TestCaseTest
+from base.test_suite import TestSuite
+from tests.test_suite_test import TestSuiteTest
+
+# ------- Questão 2 -------
+
+# test = MyTest('test_a')
+# test.run()
+
+# test = MyTest('test_b')
+# test.run()
+
+# test = MyTest('test_c')
+# test.run()
 
 # ------- Questão 3 -------
 
@@ -19,30 +32,51 @@ from tests.test_case_test import TestCaseTest
 
 # ------- Questão 4 -------
 
+# result = TestResult()
+
+# test = TestCaseTest('test_result_success_run')
+# test.run(result)
+
+# test = TestCaseTest('test_result_failure_run')
+# test.run(result)
+
+# test = TestCaseTest('test_result_error_run')
+# test.run(result)
+
+# test = TestCaseTest('test_result_multiple_run')
+# test.run(result)
+
+# test = TestCaseTest('test_was_set_up')
+# test.run(result)
+
+# test = TestCaseTest('test_was_run')
+# test.run(result)
+
+# test = TestCaseTest('test_was_tear_down')
+# test.run(result)
+
+# test = TestCaseTest('test_template_method')
+# test.run(result)
+
+# print(result.summary())
+
+# ------- Questão 5 -------
+
 result = TestResult()
+suite = TestSuite()
 
-test = TestCaseTest('test_result_success_run')
-test.run(result)
+suite.add_test(TestCaseTest('test_result_success_run'))
+suite.add_test(TestCaseTest('test_result_failure_run'))
+suite.add_test(TestCaseTest('test_result_error_run'))
+suite.add_test(TestCaseTest('test_result_multiple_run'))
+suite.add_test(TestCaseTest('test_was_set_up'))
+suite.add_test(TestCaseTest('test_was_run'))
+suite.add_test(TestCaseTest('test_was_tear_down'))
+suite.add_test(TestCaseTest('test_template_method'))
 
-test = TestCaseTest('test_result_failure_run')
-test.run(result)
+suite.add_test(TestSuiteTest('test_suite_size'))
+suite.add_test(TestSuiteTest('test_suite_success_run'))
+suite.add_test(TestSuiteTest('test_suite_multiple_run'))
 
-test = TestCaseTest('test_result_error_run')
-test.run(result)
-
-test = TestCaseTest('test_result_multiple_run')
-test.run(result)
-
-test = TestCaseTest('test_was_set_up')
-test.run(result)
-
-test = TestCaseTest('test_was_run')
-test.run(result)
-
-test = TestCaseTest('test_was_tear_down')
-test.run(result)
-
-test = TestCaseTest('test_template_method')
-test.run(result)
-
+suite.run(result)
 print(result.summary())
---
+++ src/tests/test_suite_test.py
@@ -0,0 +1,35 @@
+from base.test_case import TestCase
+from base.test_suite import TestSuite
+from base.test_result import TestResult
+from tests.test_stub import TestStub
+
+class TestSuiteTest(TestCase):
+
+    def test_suite_size(self):
+        suite = TestSuite()
+
+        suite.add_test(TestStub('test_success'))
+        suite.add_test(TestStub('test_failure'))
+        suite.add_test(TestStub('test_error'))
+
+        assert len(suite.tests) == 3
+
+    def test_suite_success_run(self):
+        result = TestResult()
+        suite = TestSuite()
+        suite.add_test(TestStub('test_success'))
+
+        suite.run(result)
+
+        assert result.summary() == '1 run, 0 failed, 0 error'
+
+    def test_suite_multiple_run(self):
+        result = TestResult()
+        suite = TestSuite()
+        suite.add_test(TestStub('test_success'))
+        suite.add_test(TestStub('test_failure'))
+        suite.add_test(TestStub('test_error'))
+
+        suite.run(result)
+
+        assert result.summary() == '3 run, 1 failed, 1 error'
