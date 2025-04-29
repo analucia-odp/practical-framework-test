e313ddbfc579c66a95f4c7d9cfab6ab6a9f79ca3
analucia-odp <analucia.odp@gmail.com>
2025-04-28 21:31:01 -0300

    6. Classes TestLoader e TestRunner

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from base.test_result import TestResult
 from tests.test_case_test import TestCaseTest
 from base.test_suite import TestSuite
 from tests.test_suite_test import TestSuiteTest
+from base.test_loader import TestLoader
+from tests.test_loader_test import TestLoaderTest
 
 # ------- Questão 2 -------
 
@@ -62,21 +64,29 @@ from tests.test_suite_test import TestSuiteTest
 
 # ------- Questão 5 -------
 
+# result = TestResult()
+# suite = TestSuite()
+
+# suite.add_test(TestCaseTest('test_result_success_run'))
+# suite.add_test(TestCaseTest('test_result_failure_run'))
+# suite.add_test(TestCaseTest('test_result_error_run'))
+# suite.add_test(TestCaseTest('test_result_multiple_run'))
+# suite.add_test(TestCaseTest('test_was_set_up'))
+# suite.add_test(TestCaseTest('test_was_run'))
+# suite.add_test(TestCaseTest('test_was_tear_down'))
+# suite.add_test(TestCaseTest('test_template_method'))
+
+# suite.add_test(TestSuiteTest('test_suite_size'))
+# suite.add_test(TestSuiteTest('test_suite_success_run'))
+# suite.add_test(TestSuiteTest('test_suite_multiple_run'))
+
+# suite.run(result)
+# print(result.summary())
+
+# ------- Questão 6 -------
+
 result = TestResult()
-suite = TestSuite()
-
-suite.add_test(TestCaseTest('test_result_success_run'))
-suite.add_test(TestCaseTest('test_result_failure_run'))
-suite.add_test(TestCaseTest('test_result_error_run'))
-suite.add_test(TestCaseTest('test_result_multiple_run'))
-suite.add_test(TestCaseTest('test_was_set_up'))
-suite.add_test(TestCaseTest('test_was_run'))
-suite.add_test(TestCaseTest('test_was_tear_down'))
-suite.add_test(TestCaseTest('test_template_method'))
-
-suite.add_test(TestSuiteTest('test_suite_size'))
-suite.add_test(TestSuiteTest('test_suite_success_run'))
-suite.add_test(TestSuiteTest('test_suite_multiple_run'))
-
+loader = TestLoader()
+suite = loader.make_suite(TestLoaderTest)
 suite.run(result)
 print(result.summary())
